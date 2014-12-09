9f06bda6a98f1e5fd0a78ea4737aaf14eb00ee3c
Nathan Carter <nc8510@mcla.edu>
2014-12-09 12:30:32 -0500

    Finished Filling in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'signup' => 'static_pages#signup'
   get 'login' => 'static_pages#login'
   get 'draft' => 'static_pages#draft'
   get 'roster' => 'static_pages#roster'
-
+  get 'signup' => 'users#new'
 end
